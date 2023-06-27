39972338ae7dcfba15f93b950588e99dd58955dd
LouisHyt <louishayotte@gmail.com>
2023-06-27 16:58:29 +0200

    Deleting javascript (to remake without templates)

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-const addTodo = document.querySelector("#btn");
-//const todoCard = document.querySelector(".todoCard");
-const templateCard = document.querySelector(".templateCard");
-const todoCardsContainer = document.querySelector(".todoCards");
-
-addTodo.addEventListener("click", () => {
-    let newTodo = templateCard.content.cloneNode(true);
-    todoCardsContainer.appendChild(newTodo);
-    button.addEventListener("click", () => {
-        newTodo.remove();
-    })
-})
+//A refaire sans template;
